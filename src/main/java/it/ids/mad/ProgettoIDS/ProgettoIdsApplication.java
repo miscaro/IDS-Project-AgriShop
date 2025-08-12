7ca9f2161443d889103d040fdaa3720dc6104735
Antonio <antonio.pennino@studenti.unicam.it>
2025-08-12 10:30:00 +0200

    Inizializzazione Spring Boot
    Commit dei file di base generati da Spring Initializr.

---
+++ src/main/java/it/ids/mad/ProgettoIDS/ProgettoIdsApplication.java
@@ -0,0 +1,13 @@
+package it.ids.mad.ProgettoIDS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoIdsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoIdsApplication.class, args);
+	}
+
+}
