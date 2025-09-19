e6c1c6bf5036f04bbfa19d7af3c798f9e45c9e37
D4mi4n0 <damiano.montanaro@studenti.unicam.it>
2025-09-19 18:45:19 +0200

    Commit di Utente.java
    Commit dei file di base e della classe Utente.java

---
+++ src/test/java/it/ids/mad/ProgettoIDS/ProgettoIdsApplicationTests.java
@@ -0,0 +1,13 @@
+package it.ids.mad.ProgettoIDS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoIdsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
