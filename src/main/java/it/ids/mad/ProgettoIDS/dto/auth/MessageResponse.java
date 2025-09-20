1bb8180b58200962ed52a302321be25c905c68f3
D4mi4n0 <damiano.montanaro@studenti.unicam.it>
2025-09-20 12:38:01 +0200

    Commit di auth
    Implementazione delle classi per l'accesso/registrazione all'app

---
+++ src/main/java/it/ids/mad/ProgettoIDS/dto/auth/MessageResponse.java
@@ -0,0 +1,8 @@
+package it.ids.mad.ProgettoIDS.dto.auth;
+
+public class MessageResponse {
+    private String message;
+    public MessageResponse(String message) { this.message = message; }
+    public String getMessage() { return message; }
+    public void setMessage(String message) { this.message = message; }
+}
---
+++ src/main/java/it/ids/mad/ProgettoIDS/dto/auth/SignupRequest.java
@@ -0,0 +1,98 @@
+package it.ids.mad.ProgettoIDS.dto.auth;
+
+import java.util.Set;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class SignupRequest {
+    @NotBlank
+    @Size(max = 20)
+    private String username;
+
+    @NotBlank
+    @Size(max = 50)
+    @Email
+    private String email;
+
+    private Set<String> role;
+
+    @NotBlank
+    @Size(min = 6, max = 40)
+    private String password;
+
+    private String ragioneSociale;
+
+    private String indirizzo;
+    private Double lat;
+    private Double lon;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getTipoUtente() {
+        return role != null && !role.isEmpty() ? role.iterator().next() : null;
+    }
+
+    public Set<String> getRole() {
+        return role;
+    }
+
+    public void setRole(Set<String> role) {
+        this.role = role;
+    }
+
+    public String getRagioneSociale() {
+        return ragioneSociale;
+    }
+
+    public void setRagioneSociale(String ragioneSociale) {
+        this.ragioneSociale = ragioneSociale;
+    }
+
+    public String getIndirizzo() {
+        return indirizzo;
+    }
+
+    public void setIndirizzo(String indirizzo) {
+        this.indirizzo = indirizzo;
+    }
+
+    public Double getLat() {
+        return lat;
+    }
+
+    public void setLat(Double lat) {
+        this.lat = lat;
+    }
+
+    public Double getLon() {
+        return lon;
+    }
+
+    public void setLon(Double lon) {
+        this.lon = lon;
+    }
+}
