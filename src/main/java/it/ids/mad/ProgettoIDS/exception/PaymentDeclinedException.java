373efdf660514c548d4bcd8f4871d7ab074fa3df
D4mi4n0 <damiano.montanaro@studenti.unicam.it>
2025-09-23 11:30:00 +0200

    Commit delle eccezioni
    Commit delle eccezioni dell'applicazione

---
+++ src/main/java/it/ids/mad/ProgettoIDS/exception/PaymentDeclinedException.java
@@ -0,0 +1,5 @@
+package it.ids.mad.ProgettoIDS.exception;
+
+public class PaymentDeclinedException extends RuntimeException {
+    public PaymentDeclinedException(String message) { super(message); }
+}
